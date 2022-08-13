67a294b06d2f087178c5acfed06f0582cdfecef7
Jaydon <pearcejaydon@gmail.com>
2022-08-13 14:55:16 +1000

    Add remove book function

--- script.js
+++ script.js
@@ -5,22 +5,13 @@ const addBookForm = document.getElementById('add-book-form');
 const overlay = document.getElementById('overlay');
 let myLibrary = [];
 
-addBookForm.onsubmit = addBook;
+addBookForm.onsubmit = addBookToLibrary;
 
 const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", "295 pages", "not read yet");
 
-addBookToLibrary(theHobbit);
+myLibrary.push(theHobbit);
 
 displayBooks();
-
-function addBook(e){
-    e.preventDefault();
-    const newBook = getBookFromInput();
-
-    myLibrary.push(newBook);
-    closeModal(addBookForm.closest('.modal'));
-    displayBooks();
-}
 
 const getBookFromInput = () => {
     const title = document.getElementById('form-book-title').value;
@@ -75,8 +66,13 @@ Book.prototype.info = function(){
     return `${this.title} by ${this.author}, ${this.page}, ${this.read}`;
 }
 
-function addBookToLibrary(book){
-    myLibrary.push(book);
+function addBookToLibrary(e){
+    e.preventDefault();
+    const newBook = getBookFromInput();
+
+    myLibrary.push(newBook);
+    closeModal(addBookForm.closest('.modal'));
+    displayBooks();
 }
 
 function clearBookDisplay(){
@@ -92,19 +88,35 @@ function displayBooks(){
         const pages = document.createElement('h3');
         const read = document.createElement('p');
         const readBtn = document.createElement('button');
+        const removeBtn = document.createElement('button');
 
         card.classList.add('card');
+        card.dataset.bookId = i;
         title.innerText = book.title;
         author.innerText = book.author;
         pages.innerText = book.page;
         book.read ? read.innerHTML = "Read" :read.innerHTML = "Not read"
         readBtn.innerText = "Toggle read";
+        readBtn.classList.add("read-btn");
+        removeBtn.innerText = "Remove Book";
+        removeBtn.classList.add('remove-btn');
+        removeBtn.dataset.bookId = i;
 
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(pages);
         card.appendChild(read);
         card.appendChild(readBtn);
+        card.appendChild(removeBtn);
         cardWrapper.appendChild(card);
     });
 }
+
+//using event bubbling to find remove btn or toggle read
+cardWrapper.addEventListener('click', (e) => {
+    if(e.target.classList.contains('remove-btn')){
+        myLibrary.splice(e.target.dataset.bookId, 1);
+        displayBooks();
+    }
+    //TODO: Toggle Read
+})
